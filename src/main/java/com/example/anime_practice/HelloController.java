f54fdeed6f7745bde3a9f49b9964e0953bed9539
Choi TaeSan <xotks7524@gmail.com>
2023-12-04 11:20:46 +0900

    Feat: first commit - 애니메이션 예제 기능 빌드

---
+++ src/main/java/com/example/anime_practice/HelloController.java
@@ -0,0 +1,48 @@
+package com.example.anime_practice;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.URL;
+import java.security.Key;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
+    @FXML
+    private BorderPane main;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        main.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                try {
+                    Parent sub = FXMLLoader.load(getClass().getResource("sub.fxml"));
+                    StackPane root = (StackPane) main.getScene().getRoot();
+                    root.getChildren().add(sub);
+
+                    sub.setTranslateX(400);
+
+                    Timeline timeline = new Timeline();
+                    KeyValue keyValue = new KeyValue(sub.translateXProperty(),0);
+                    KeyFrame keyFrame = new KeyFrame(Duration.millis(100), keyValue);
+                    timeline.getKeyFrames().add(keyFrame);
+                    timeline.play();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
+}
---
+++ src/main/java/com/example/anime_practice/SubController.java
@@ -0,0 +1,13 @@
+package com.example.anime_practice;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SubController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
